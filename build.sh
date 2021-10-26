34c641fd21438ac35b74cd870e952e667b93582c
zifengqi123 <zifengqi123@sina.com>
2021-10-26 18:57:00 +0800

    delete addon/nuttx-export-10.1.0-RC1/ && rebuild

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@ make clean
 make
 make export
 cd addon/
-rm nuttx-export-10.1.0-RC1* -rf
-mv ../nuttx-export-10.1.0-RC1.zip .
-unzip nuttx-export-10.1.0-RC1.zip
+rm nuttx-export-10.1.0* -rf
+mv ../nuttx-export-10.1.0.zip .
+unzip nuttx-export-10.1.0.zip
+
 cd ledcontrol/
 make clean
 make
